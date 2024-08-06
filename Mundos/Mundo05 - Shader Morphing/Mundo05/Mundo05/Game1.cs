00717f1213253698ecfad8b28146faaa1300ecdd
FernandoBMS <fernando.bentomouradesouza@gmail.com>
2024-08-06 16:00:59 -0300

    pixel shader morphing example, mundo 5

---
+++ Mundos/Mundo05 - Shader Morphing/Mundo05/Mundo05/Game1.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Mundo05
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Random random;
+
+        Screen screen;
+        Camera camera;
+
+        Quad plane;
+        Cube house;
+        Windmill[] windmills;
+
+        List<GameObject> colliders;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+
+            IsMouseVisible = true;
+            Window.Title = "MUNDO 05";
+        }
+
+        protected override void Initialize()
+        {
+            screen = Screen.GetInstance();
+            screen.Width = graphics.PreferredBackBufferWidth;
+            screen.Height = graphics.PreferredBackBufferHeight;
+
+            int seed = (int)DateTime.Now.Ticks % int.MaxValue;
+            random = new Random(seed);
+
+            camera = new Camera(this);
+
+            plane = new Quad(this);
+            house = new Cube(this);
+            windmills = new Windmill[]
+            {
+                new Windmill(this, random.Next(1,10)),
+                new Windmill(this, random.Next(1,10)),
+            };
+
+            plane.Scale = new Vector3(20, 1, 20);
+            house.Position = new Vector3(0, 1, 0);
+
+            windmills[0].Position = new Vector3(-8, 2,0);
+            windmills[1].Position = new Vector3( 8, 2,0);
+
+            windmills[0].Rotation = new Vector3(0, MathHelper.ToRadians(45), 0);
+            windmills[1].Rotation = new Vector3(0, MathHelper.ToRadians(-45), 0);
+
+            colliders = new List<GameObject>() { plane, house, windmills[0], windmills[1] };
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            camera.Update(gameTime);
+
+            foreach(Windmill windmill in windmills) 
+                windmill.Update(gameTime);
+
+            foreach (GameObject obj in colliders)
+            {
+                if (camera.IsColliding(obj.BBox))
+                {
+                    camera.RestorePosition();
+                    obj.SetColliderColor(Color.Red);
+                }
+                else obj.SetColliderColor(Color.Green);
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // RasterizerState rs = new RasterizerState();
+            //rs.CullMode = CullMode.None;
+            //rs.FillMode = FillMode.WireFrame;
+            // GraphicsDevice.RasterizerState = rs;
+
+            plane.Draw(camera, gameTime);
+            house.Draw(camera, gameTime);
+            foreach (Windmill windmill in windmills)
+                windmill.Draw(camera, gameTime);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Mundos/Mundo05 - Shader Morphing/Mundo05/Mundo05/GameObject.cs
@@ -0,0 +1,175 @@
+﻿#define USE_TEXTURE
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mundo05
+{
+    public abstract class GameObject
+    {
+        private VertexBuffer buffer;
+        protected Effect effect;
+        private Game game;
+
+        private Vector3 size;
+        private Vector3 position;
+        private Vector3 rotation;
+        private Vector3 scale;
+
+        public Vector3 Size
+        {
+            get { return size; }
+            protected set 
+            {
+                size = value;
+                LBox = new LineBox(game, size/scale, Color.Green);
+                UpdateBoundingBox(Position, size);
+            }
+        }
+        public Vector3 Position
+        {
+            get { return position; }
+            set
+            {
+                position = value;
+                UpdateBoundingBox(position, Size);
+            }
+        }
+        public Vector3 Rotation
+        {
+            get { return rotation; }
+            set
+            {
+                rotation = value;
+                UpdateBoundingBox(Position, Size);                                      
+            }
+        }
+        public Vector3 Scale
+        {
+            get { return scale; }
+            set
+            {
+                scale = value;
+                Size *= scale;
+            }
+        }
+
+        public LineBox LBox { get; protected set; }
+        public BoundingBox BBox { get; private set; }
+#if USE_TEXTURE
+        protected Texture2D Texture { get; set; }
+        protected Texture2D TextureWinter { get; set; }
+        protected VertexPositionTexture[] Vertices { get; set; }
+#else
+        protected VertexPositionColor[] Vertices { get; set; }
+#endif
+
+        public GameObject(Game game)
+        {
+            this.game = game;
+            Vertices = null;
+            Texture = null;
+            TextureWinter = null;
+
+            if (Vertices != null)
+            {
+#if USE_TEXTURE
+                buffer = new VertexBuffer(game.GraphicsDevice,
+                                          typeof(VertexPositionTexture),
+                                          Vertices.Length,
+                                          BufferUsage.None);
+                buffer.SetData<VertexPositionTexture>(Vertices);
+#else
+                buffer = new VertexBuffer(game.GraphicsDevice, 
+                                          typeof(VertexPositionColor), 
+                                          Vertices.Length, 
+                                          BufferUsage.None);
+                buffer.SetData<VertexPositionColor>(Vertices);
+#endif
+            }
+
+            effect = game.Content.Load<Effect>(@"Effects\morphing");
+
+            Scale = Vector3.One;
+            Rotation = Vector3.Zero;
+            Position = Vector3.Zero;
+
+            Size = Vector3.One;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        { 
+            
+        }
+
+        public void Draw(Camera camera, GameTime gameTime, bool showColliders = false)
+        {
+            Draw(camera, Matrix.Identity, gameTime, showColliders);
+        }
+        
+        public virtual void Draw(Camera camera, Matrix parentWorld, GameTime gameTime, bool showColliders = false)
+        {
+            if (Vertices != null)
+                game.GraphicsDevice.SetVertexBuffer(buffer);
+
+            Matrix localMatrix = Matrix.CreateScale(Scale)
+                                 * Matrix.CreateFromYawPitchRoll(Rotation.Y, Rotation.X, Rotation.Z)
+                                 * Matrix.CreateTranslation(Position);
+
+            Matrix result = localMatrix * parentWorld;
+
+            effect.CurrentTechnique = effect.Techniques["Technique1"];
+            effect.Parameters["World"].SetValue(result);
+            effect.Parameters["View"].SetValue(camera.View);
+            effect.Parameters["Projection"].SetValue(camera.Projection);
+            
+#if USE_TEXTURE
+            effect.Parameters["normalTexture"].SetValue(Texture);
+            effect.Parameters["winterTexture"].SetValue(TextureWinter);
+            float gt = (float)(gameTime.TotalGameTime.TotalSeconds % 10) / 10;
+            effect.Parameters["gameTime"].SetValue(gt);
+
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                if (Vertices != null)
+                    game.GraphicsDevice.DrawUserPrimitives<VertexPositionTexture>(PrimitiveType.TriangleList,
+                                                                   Vertices,
+                                                                   0,
+                                                                   Vertices.Length / 3);
+            }
+            //effect.TextureEnabled = false;
+#else
+            effect.VertexColorEnabled = true;
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                if (Vertices != null)
+                    game.GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.TriangleList,
+                                                                   Vertices,
+                                                                   0,
+                                                                   Vertices.Length / 3);
+            }
+            effect.VertexColorEnabled = false;
+#endif
+
+            if (showColliders) LBox.Draw(result, camera);
+        }
+
+        public void UpdateBoundingBox(Vector3 position, Vector3 size)
+        {
+            BBox = new BoundingBox(position - (size / 2f),
+                                   position + (size / 2f));
+        }
+
+        public bool IsColliding(BoundingBox other)
+        {
+            return BBox.Intersects(other);
+        }
+
+        public void SetColliderColor(Color color)
+        {
+            LBox.SetColor(color);
+        }
+    }
+}
