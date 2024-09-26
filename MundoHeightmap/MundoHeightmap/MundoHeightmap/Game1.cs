574c96c170b47d1aa017c76c168c29acf6a5e305
fernando_pc <fernando.bentomouradesouza@gmail.com>
2024-09-26 15:40:51 -0300

    iniciando meu heightmap

---
+++ MundoHeightmap/MundoHeightmap/MundoHeightmap/Game1.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace MundoHeightmap
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Camera camera;
+        Screen screen;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            Window.Title = "HEIGHTMAP";
+            IsMouseVisible = true;
+
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+        }
+
+        protected override void Initialize()
+        {
+            screen = Screen.GetInstance();
+            screen.Width = graphics.PreferredBackBufferWidth;
+            screen.Height = graphics.PreferredBackBufferHeight;
+
+            camera = new Camera(this);
+
+            //RasterizerState rs = new RasterizerState();
+            //rs.FillMode = FillMode.WireFrame;
+            //GraphicsDevice.RasterizerState = rs;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            camera.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MundoHeightmap/MundoHeightmap/MundoHeightmap/Heightmap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MundoHeightmap
+{
+    public class Heightmap
+    {
+        // 1 - montar um quadrado com index (use o codigo do linebox da aula de colisao para ajudar a entender)
+        // 2 - montar um cubo com index (use o codigo do linebox da aula de colisao para ajudar a entender)
+        // 3 - montar um grid com index
+        // 4 - aplicar o heightmap no grid (esquema com as cores da textura alterando o y dos vertices)
+        // 5 - mostrar as normais (apontando pra cima)
+        // 6 - recalcular as normais (apontar para a direcao correta)
+        // 7 - normalizar normais para nao ficar linhas grandes
+    }
+}
